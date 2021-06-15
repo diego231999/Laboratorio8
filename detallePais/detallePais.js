8abfdc8f17e637f86576503bc11c94b2db7e4ab8
JohamRV <a20181459@pucp.edu.pe>
2021-06-15 10:37:12 -0500

    obtener los parámetros por la URL

--- detallePais/detallePais.js
+++ detallePais/detallePais.js
@@ -1,19 +1,29 @@
 
+// permite obtener datos de la URL 'windom.location-search <- llama a la url'
+// se instancia en un objeto urlParams
 const urlParams = new URLSearchParams(window.location.search);
+//obteniendo un parametro del url
+// name = valor.Country
+// slug = valor.Slug
+// countryCode = valor.CountryCode
+// caseCovid = confirmed <- TODO
 const nameCountry = urlParams.get('name');
-//TODO
+const slug = urlParams.get('slug');
+const countryCode = urlParams.get('countryCode');
+//const caseCovid = urlParams.get('caseCovid');
+const caseCovid = 'confirmed';
 
 $(document).ready(function () {
     // const caseCovid = 'confimed';
     $("#titulo").html('Resumen del país ' + nameCountry);
-    //TODO
+    $("#bandera-div").html()
 
     $.ajax({
         method: "GET",
         datatype: "json",
         url: "https://restcountries.eu/rest/v2/alpha/" + countryCode
     }).done(function (data) {
-        //TODO
+        //TODO obtener la imagen de la bandera en base a su id
     }).fail(function (err) {
         console.log(err);
         alert("ocurrió un error al cargar la página");
