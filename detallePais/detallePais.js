42874aca05fdfb503182cdada62220d02bff79a2
JohamRV <a20181459@pucp.edu.pe>
2021-06-15 19:22:59 -0500

    detalle de país - 50% correcion index.html -> error al enviar valores en algunos países

--- detallePais/detallePais.js
+++ detallePais/detallePais.js
@@ -10,20 +10,27 @@ const urlParams = new URLSearchParams(window.location.search);
 const nameCountry = urlParams.get('name');
 const slug = urlParams.get('slug');
 const countryCode = urlParams.get('countryCode');
-//const caseCovid = urlParams.get('caseCovid');
-const caseCovid = 'confirmed';
-
+const caseCovid = urlParams.get('caseCovid');
+console.log(nameCountry);
+console.log(slug);
+console.log(countryCode);
+console.log(caseCovid);
 $(document).ready(function () {
     // const caseCovid = 'confimed';
     $("#titulo").html('Resumen del país ' + nameCountry);
-    $("#bandera-div").html()
-
     $.ajax({
         method: "GET",
         datatype: "json",
         url: "https://restcountries.eu/rest/v2/alpha/" + countryCode
     }).done(function (data) {
-        //TODO obtener la imagen de la bandera en base a su id
+        $("#bandera-div").after(
+            "<img  src= " + data.flag + ">"
+        );
+
+        $("#capital").html(data.name);
+        $("#population").html(data.population);
+        $("#subregion").html(data.subregion);
+
     }).fail(function (err) {
         console.log(err);
         alert("ocurrió un error al cargar la página");
@@ -36,13 +43,21 @@ function seleccionarCasos() {
     //TODO
 }
 
+// se muestra la tabla de casos
 function obtenerDataPais() {
     $.ajax({
         method: "GET",
         datatype: "json",
         url: "https://api.covid19api.com/total/dayone/country/" + slug + "/status/" + caseCovid
     }).done(function (data) {
-        //TODO
+        let contentHTML = "";
+        $.each(data, function (i, casoCovid){
+            let formatDate = formatDate(casoCovid["Date"]);
+           contentHTML += "<tr>";
+           contentHTML += "<td>"+formatDate+"</td>";
+           contentHTML += "<td>"+casoCovid["Cases"]+"</td>";
+           contentHTML += "</tr>";
+        });
     }).fail(function (err) {
         console.log(err);
         alert("ocurrió un error al cargar la página");
@@ -50,5 +65,101 @@ function obtenerDataPais() {
 }
 
 function formatDate(date) {
-    //TODO
+  /*  DateFormat hourdateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    hourdateFormat.date;*/
 }
+
+/*
+* {
+    "name": "Argentina",
+    "topLevelDomain": [
+        ".ar"
+    ],
+    "alpha2Code": "AR",
+    "alpha3Code": "ARG",
+    "callingCodes": [
+        "54"
+    ],
+    "capital": "Buenos Aires",
+    "altSpellings": [
+        "AR",
+        "Argentine Republic",
+        "República Argentina"
+    ],
+    "region": "Americas",
+    "subregion": "South America",
+    "population": 43590400,
+    "latlng": [
+        -34.0,
+        -64.0
+    ],
+    "demonym": "Argentinean",
+    "area": 2780400.0,
+    "gini": 44.5,
+    "timezones": [
+        "UTC-03:00"
+    ],
+    "borders": [
+        "BOL",
+        "BRA",
+        "CHL",
+        "PRY",
+        "URY"
+    ],
+    "nativeName": "Argentina",
+    "numericCode": "032",
+    "currencies": [
+        {
+            "code": "ARS",
+            "name": "Argentine peso",
+            "symbol": "$"
+        }
+    ],
+    "languages": [
+        {
+            "iso639_1": "es",
+            "iso639_2": "spa",
+            "name": "Spanish",
+            "nativeName": "Español"
+        },
+        {
+            "iso639_1": "gn",
+            "iso639_2": "grn",
+            "name": "Guaraní",
+            "nativeName": "Avañe'ẽ"
+        }
+    ],
+    "translations": {
+        "de": "Argentinien",
+        "es": "Argentina",
+        "fr": "Argentine",
+        "ja": "アルゼンチン",
+        "it": "Argentina",
+        "br": "Argentina",
+        "pt": "Argentina",
+        "nl": "Argentinië",
+        "hr": "Argentina",
+        "fa": "آرژانتین"
+    },
+    "flag": "https://restcountries.eu/data/arg.svg",
+    "regionalBlocs": [
+        {
+            "acronym": "USAN",
+            "name": "Union of South American Nations",
+            "otherAcronyms": [
+                "UNASUR",
+                "UNASUL",
+                "UZAN"
+            ],
+            "otherNames": [
+                "Unión de Naciones Suramericanas",
+                "União de Nações Sul-Americanas",
+                "Unie van Zuid-Amerikaanse Naties",
+                "South American Union"
+            ]
+        }
+    ],
+    "cioc": "ARG"
+}
+*
+* */
--- index.js
+++ index.js
@@ -4,24 +4,43 @@ $(document).ready(function () {
         method: "GET",
         datatype: "json",
         crossDomain: true,
-        headers: {"ID": "ba75da48-8a51-46f4-b3c4-69da5690949d"},
+        //headers: {"ID": "ba75da48-8a51-46f4-b3c4-69da5690949d"},
+        headers: {"ID": "bd26f79a-c71e-46bf-ad80-86af436802fb"},
         url: "https://api.covid19api.com/summary"
     }).done(function (data) {
-        console.log(data);
-        $("#newConfirmed").val(data.Global.NewConfirmed);
-        $("#newDeaths").val(data.Global.NewDeaths);
-        $("#newRecovered").val(data.Global.NewRecovered);
-        $("#totalConfirmed").val(data.Global.TotalConfirmed);
-        $("#totalDeaths").val(data.Global.TotalDeaths);
-        $("#totalRecovered").val(data.Global.TotalRecovered);
+
+    /*    const global = {
+            "#newConfirmed": data.Global.NewConfirmed,
+            "#newDeaths": data.Global.NewDeaths,
+            "#newRecovered": data.Global.NewRecovered,
+            "#totalConfirmed": data.Global.TotalConfirmed,
+            "#totalDeaths": data.Global.TotalDeaths,
+            "#totalRecovered": data.Global.TotalRecovered
+        };
+
+        for (let key of global){
+            $(key).val(global[key]).html(global[key]);
+        }
+*/
+        $("#newConfirmed").val(data.Global.NewConfirmed).html(data.Global.NewConfirmed);
+        $("#newDeaths").val(data.Global.NewDeaths).html(data.Global.NewDeaths);
+        $("#newRecovered").val(data.Global.NewRecovered).html(data.Global.NewRecovered);
+        $("#totalConfirmed").val(data.Global.TotalConfirmed).html(data.Global.TotalConfirmed);
+        $("#totalDeaths").val(data.Global.TotalDeaths).html(data.Global.TotalDeaths);
+        $("#totalRecovered").val(data.Global.TotalRecovered).html(data.Global.TotalRecovered);
+
         var date= data.Global.Date;
         var fecha=formatDate(date);
-        $("#titulo-resumen-global").val(fecha);
+        $("#titulo-resumen-global").html("Resumen global al "+fecha);
 
         var listaPaises= data.Countries;
-        var listaPaisesSort=listaPaises.sort(compare);
+        var listaPaisesSort=listaPaises.sort(sortingJson("TotalConfirmed"));
         var contentHtml="";
+        var url = window.location.href;
+
         $.each(listaPaisesSort, function (i, valor) {
+            var nextPath = "detallePais/detallePais.html?name="+valor["Country"]+"&slug="+valor["Slug"]+"&countryCode="+valor["CountryCode"]+"&caseCovid=confirmed";
+            var newUrl = url.replace("index.html",nextPath);
             contentHtml += "<tr>";
             contentHtml += "<td>" + (i + 1) + "</td>";
             contentHtml += "<td>" + valor.Country + "</td>";
@@ -31,27 +50,44 @@ $(document).ready(function () {
             contentHtml += "<td>" + valor.NewConfirmed + "</td>";
             contentHtml += "<td>" + valor.NewDeaths + "</td>";
             contentHtml += "<td>" + valor.NewRecovered + "</td>";
-            contentHtml+= "<button onclick='obtenerDataPais();'"+"onclick="+"location.href='/detallePais/detallePais.html?name="+valor.Country+"&slug="+valor.Slug+"&countryCode="+valor.CountryCode+"&caseCovid=confirmed'"+">Enviar</button>"
-
-        });
+            contentHtml+= "<td><a class='btn btn-primary' href="+ newUrl +">Enviar</a></td>"
+            contentHtml += "</tr>";
+        })
+        $("#body-paises").html(contentHtml);
         }).fail(function (err) {
         console.log(err);
         alert("ocurrió un error al cargar la página");
     });
 });
 
-function compare(a, b) {
+/*function compare(a, b) {
     // Use toUpperCase() to ignore character casing
+    console.log("Entro a la comparador");
     const paisA = a.TotalConfirmed;
     const paisB = b.TotalConfirmed;
     let comparison = 0;
+
+    paisA > paisB ? comparison = 1 : comparison = -1 ;
+
     if (paisA > paisB) {
         comparison=1;
     }
     if (PaisB > paisA) {
         comparision= -1;
     }
+
     return comparison;
+}*/
+function sortingJson(TotalConfirmed){
+    return function(a,b){
+        if (a[TotalConfirmed] > b[TotalConfirmed]) {
+            return 1;
+        }
+        if (b[TotalConfirmed] > a[TotalConfirmed]) {
+            return -1;
+        }
+        return 0;
+    };
 }
 
 function formatDate(date) {
